d0bcf0a4132fff1676787e4bbaa0528ebb6eb4ed
VireshMeghanathi <123720255+VireshMeghanathi@users.noreply.github.com>
2024-05-02 13:51:27 +0530

    adding modlue date.js and adding work and about renders

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
-const express = require('express');
-const BodyParser = require('body-parser');
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-app.use(BodyParser.urlencoded({ extended: true })); 
-app.use(express.static('public')); 
-app.set("view engine", "ejs");
 
-let items = ["Buy food","Cook food","eat Fooid"];
+app.set('view engine', 'ejs');
 
-app.get("/",function(req,res){
-    let today = new Date();
-    // let currentday = today.getDay();
-    // day ="";
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-    let options = {
-        weekday: "long",
-        day : "numeric",
-        month : "long"
-    }
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
-    let day = today.toLocaleDateString("en-US",options);
+app.get("/", function(req, res) {
 
-    res.render("list",{kindofday:day , newlistitems: items} );  
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
-app.post("/",function(req,res){
-       let item = req.body.todo;
-        items.push(item);
-        res.redirect('/');
-})
 
+app.post("/", function(req, res){
 
-app.listen(3000,function(){
-    console.log("app is running on 3000");
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
